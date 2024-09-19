010d1e52dd4134cdc01f65ef753af7ae68c7dd30
Débora Vázquez <debora.vazquez@gmail.com>
2024-09-20 01:48:59 +0200

    Add initial admin config

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # deezer-code-test-backend
+
+## Configuration
+
+Copy the ```.env.template``` file and rename it to ```.env```. Assign the following environment variables: 
+
+```
+SUPERUSER_USERNAME=
+SUPERUSER_EMAIL=
+SUPERUSER_PASSWORD=
+```
+
+## Installation
+
+```
+docker compose up
+```
---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
+# User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')
+
+
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+
+try:
+    superuser = User.objects.create_superuser(
+        username=os.getenv('SUPERUSER_USERNAME'),
+        email=os.getenv('SUPERUSER_EMAIL'),
+        password=os.getenv('SUPERUSER_PASSWORD'))
+    superuser.save()
+except IntegrityError:
+    print(f"Super User with username {os.getenv('SUPERUSER_USERNAME')} is already exit!")
+except Exception as e:
+    print(e)
